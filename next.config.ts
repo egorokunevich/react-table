c0b52f3b50794a20e2d35a21b1a386bf7717dcda
egorokunevich <mrcraigre@gmail.com>
2025-07-03 19:27:08 +0300

    chore: configure build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/react-table' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-table/' : '',
 };
 
 export default nextConfig;
